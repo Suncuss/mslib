8bb2e39155d123155118a4e2ab3db53af081878a
Yudong Sun <sun82@wisc.edu>
2015-07-28 14:49:23 -0500

    added new program, modified myProgs.mk

---
+++ myProgs/yudong/yudong.mk
@@ -0,0 +1,25 @@
+# UPDATED PROTOCOL FOR myProgs/USERNAME usage
+#
+# Example:
+# for objects Obj1.h/Obj1.cpp and Obj2.h/Obj2.cpp
+#
+# MYSOURCE   = Obj1 Obj2
+#
+# for programs prog1.cpp prog2.cpp
+#
+# MYPROGS    = prog1 prog2
+#
+# To compile (from the MSL/trunk directory):
+# Objects:
+#   make objs/Obj1.o
+#   make objs/Obj2.o
+#
+# Programs:
+#   make bin/prog1
+#   make bin/prog2
+
+MYDIR = myProgs/yudong
+MYSOURCE  = 
+MYPROGS   = alignHomoDimer
+MYHEADERS = 
+
